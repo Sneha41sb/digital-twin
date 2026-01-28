8ef298dc8e7eb89e9ad2a5f6b41272af2b30b042
Sneha S Biju <snehasbiju2004@gmail.com>
2026-01-28 18:36:38 +0000

    Sensor noise and mechanical temperature-vibration coupling

--- main.go
+++ main.go
@@ -15,13 +15,26 @@ func main(){
 	rand.Seed(time.Now().UnixNano())
 	motor := Motor{
 		ID: 1,
-		Temperature: 0.0,
-		Vibration: 0.0,
-		RPM: 0,
+		Temperature: 60.0,
+		Vibration: 1.0,
+		RPM: 1200,
 	}
 	for {
-		motor.Temperature=60+ rand.ExpFloat64()*40
-		motor.Vibration=rand.Float64()*5
+		tempNoise := rand.Float64()*2-1
+		motor.Temperature+=tempNoise
+		if(motor.Temperature<40){
+			motor.Temperature=40
+		}
+		if(motor.Temperature>80){
+			motor.Temperature=80
+		}
+		motor.Vibration+=rand.Float64()*0.2-0.1
+		if(motor.Temperature>75){
+			motor.Vibration +=0.5
+		}
+		if(motor.Vibration<0){
+			motor.Vibration=0
+		}
 		motor.RPM=900+rand.Intn(600)
 	
 	fmt.Printf(
